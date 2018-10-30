773e78b9a16d42fb7db750f27d0dd879ae193680
cristian <thepany96@gmail.com>
2018-10-30 18:59:19 -0400

    first commit

---
+++ routes/botman.php
@@ -0,0 +1,9 @@
+<?php
+use App\Http\Controllers\BotManController;
+
+$botman = resolve('botman');
+
+$botman->hears('Hi', function ($bot) {
+    $bot->reply('Hello!');
+});
+$botman->hears('Iniciar conversacion', BotManController::class.'@startConversation');
